d105283746e56b6dbd86a05dfd094af49568794b
pietromosca1994 <pietromosca1994@gmail.com>
2021-02-15 08:20:41 +0100

    added the folder src

---
+++ src/DP.m
@@ -0,0 +1,145 @@
+%% Implementation of 1-D Dynamic Programming framework
+%% Author: Pietro Mosca
+%% Email: pietromosca1994@gmail.com
+%% Date: 04.02.2021
+
+classdef DP<handle
+
+    properties
+        Jtogo;      % array(time, n_dim)      cost-to-go table            
+        u_opt;      % array(time, n_dim)      optimal policy
+        time;       % array(time)             time array
+        DX;         % array(n_states)         discrete states
+        DU;         % array(n_inputs)         discrete input
+        log;        % struct                  log
+        X_opt       % array(time, n_inputs)   optimal state trajectory
+    
+    end % end of properties
+    
+    
+    methods 
+        %% function to initialize the DP 
+        % grd           struct     grid definition
+        % prb           struct     problem description
+        % options       struct     options          
+        
+        function init(obj, grd, prb, options)
+            obj.time=prb.start_T:prb.Ts:prb.end_T;
+            obj.DX=linspace(grd.Xn.lo, grd.Xn.hi, grd.Nx);                        
+            obj.DU=linspace(grd.Un.lo, grd.Un.hi, grd.Nu);
+            
+            % initialize cost-to-go grid
+            % scale to n dim
+            obj.Jtogo=zeros(numel(obj.time), grd.Nx);
+            % initialize  grid
+            obj.u_opt=zeros(size(obj.Jtogo));
+            
+            % initialize forward simulation
+            obj.X_opt=zeros(size(obj.time));          
+            
+            % final cost initialization
+            % scale to ndim
+            if isempty(options.gN)
+             obj.Jtogo(end, (obj.DX>grd.XN.hi | obj.DX<grd.XN.lo))=prb.InfCost;
+             obj.Jtogo(end, (obj.DX<grd.XN.hi & obj.DX>grd.XN.lo))=prb.G(obj.DX(obj.DX<grd.XN.hi & obj.DX>grd.XN.lo));           
+            else
+              obj.Jtogo=options.gN;
+            end
+            
+            if options.log==true
+              obj.log.jtogo=zeros(numel(obj.time), numel(obj.DX), numel(obj.DU));
+              obj.log.elapsed_time=zeros(numel(obj.time)-1);
+            end
+        end
+        
+        %% Function to compute the Jtogo for a time step
+        % T_step        time step
+        % prb           problem description
+        % options       problem options
+        function compute_step(obj, T_step, prb, options)
+            
+            if options.verbose==true
+              disp(['[INFO] T_step:', num2str(T_step)]);
+            end        
+            
+            % loop through the states
+            for i=1:numel(obj.DX)
+                
+                if options.verbose==true
+                  if mod(i, 50)==0
+                    disp(['[INFO] State', num2str(i)]);  
+                  end
+                end
+                
+                % loop through the inputs
+                for j=1:numel(obj.DU)
+                  % group inputs
+                  inp.X=obj.DX(i);
+                  inp.U=obj.DU(j);
+                  inp.Ts=prb.Ts;
+                  
+                  % parameter definition
+                  par=[];
+                  
+                  % cost evaluation
+                  [X(j), J(j), I(j), signals]=prb.J(inp, par);
+                  
+                  if options.interp=='interp1'
+                    % interpolation on next cost-to-go vector
+                    Jtogo_t(j)=J(j)+interp1(obj.DX, obj.Jtogo(T_step+1, :), X(j));
+                  
+                  
+                  end
+                  
+                 [obj.Jtogo(T_step, i), idx]=min(Jtogo_t);
+                 obj.u_opt(T_step, i)=obj.DU(idx);
+                                
+                end
+                
+                % log result
+                if options.log==true
+                  obj.log.Jtogo(T_step, i, :)=Jtogo_t;
+                end
+                
+            end
+        end
+      
+      %% function for backpropagation 
+      % prb   struct
+      % 
+      function get_BP(obj, prb, options)
+
+        for T_step=numel(obj.time)-1:-1:1
+          waitbar((numel(obj.time)-T_step)/numel(obj.time));
+          
+          t0 = clock (); % start time
+          obj.compute_step(T_step, prb, options)
+          elapsed_time=etime (clock (), t0); % elapsed time 
+          
+          % log execution time
+          if options.log==true
+            obj.log.elapsed_time(T_step)=elapsed_time; 
+          end
+          
+        end
+      end
+      
+      %% function for forward simulation
+      
+      function forward_sim(obj, grd, prb)
+        % loop through time forward
+        obj.X_opt(1)=grd.X0;
+        
+        for T_step=1:numel(obj.time)-1
+          inp.X=obj.X_opt(T_step);
+          inp.U=interp1(obj.DX, obj.u_opt(T_step, :), inp.X);
+          inp.Ts=prb.Ts;
+          par=[];         
+          [obj.X_opt(T_step+1), ~, ~, ~]=prb.J(inp, par);
+        end
+        
+      end
+    
+    end % end of methods
+    
+end % end of class
---
+++ src/test_scripts/test_Fishery_DP.m
@@ -0,0 +1,121 @@
+%% Test function for the DP framework used to solve the fishery problem
+%% Author: Pietro Mosca
+%% Email: pietromosca1994@gmail.com
+%% Date: 04.02.2021
+
+clear all;
+
+% Fishery Problem test
+
+date='20210214';
+
+%% input definition
+% grid definition
+grd.Nx=101;
+grd.Xn.lo=0;
+grd.Xn.hi=1000;
+grd.XN.lo=750;
+grd.XN.hi=1000;
+grd.Nu=11;
+grd.Un.lo=0;
+grd.Un.hi=10;
+grd.X0=500;
+
+% problem definition
+prb.Ts=1/5;                       % float               [day]
+prb.start_T=0.4;                  % float               [day]
+prb.end_T=200;                    % float               [day]
+prb.InfCost=1000;                 % float               infinite cost
+prb.G=@(x)(0);                    % function handler    final cost function
+prb.J=@(inp, par)(fishery(inp, par));
+
+% options definition
+options.interp='interp1';   % str               interpolation method
+options.log=true;           % bool              logs intermidiate results
+options.verbose=true;       % bool              shows logs if true
+options.gN=[];              % array(n-dim)      final cost matrix
+
+%% Functionality test
+dynamic_programming=DP();
+dynamic_programming.init(grd, prb, options);
+
+T_step=1000;
+% test step
+%dynamic_programming.compute_step(T_step, prb, options);
+
+%test algorithm
+dynamic_programming.get_BP(prb, options)
+
+% forward computation
+dynamic_programming.forward_sim(grd, prb);
+
+%% result plot
+% plot final cost array
+figure();
+
+subplot(3,1,1)
+plot(dynamic_programming.DX, dynamic_programming.Jtogo(end, :));
+xlabel('X');
+ylabel('Jtogo');
+title(['Final Cost to Go']);
+xticks(dynamic_programming.DX);
+grid on;
+
+% plot jtogo for one step
+step_Jtogo=size(dynamic_programming.Jtogo,1)-1;
+
+subplot(3,1,2)
+plot(dynamic_programming.DX, dynamic_programming.Jtogo(step_Jtogo, :));
+xlabel('X');
+ylabel('Jtogo');
+title(['Jtogo @', num2str(step_Jtogo)]);
+xticks(dynamic_programming.DX);
+grid on;
+
+% plot optimal input for one step
+step_u_opt=step_Jtogo;
+
+subplot(3,1,3)
+plot(dynamic_programming.DX, dynamic_programming.u_opt(step_u_opt, :));
+xlabel('X');
+ylabel('u_opt');
+title(['u_o_p_t @', num2str(step_u_opt)]);
+xticks(dynamic_programming.DX);
+grid on;
+
+%
+figure()
+subplot(2,1,1)
+contourf(dynamic_programming.time, dynamic_programming.DX,  dynamic_programming.Jtogo');
+colorbar;
+xlabel('Time');
+ylabel('State');
+title('J-to-go');
+
+subplot(2,1,2)
+contourf( dynamic_programming.time, dynamic_programming.DX, dynamic_programming.u_opt');
+colorbar;
+xlabel('Time');
+ylabel('State');
+title('u_o_p_t');
+
+figure();
+plot(dynamic_programming.time, dynamic_programming.X_opt);
+xlabel('Time');
+ylabel('State');
+title('Optimal State-Trajectory');
+
+% save data
+Jtogo=dynamic_programming.Jtogo;
+u_opt=dynamic_programming.u_opt;
+log=dynamic_programming.log;
+time=dynamic_programming.time;
+DX=dynamic_programming.DX;
+DU=dynamic_programming.DU;
+
+save([date, '_workspace.mat']);
+
+
+
+
+
